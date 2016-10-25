4eaa0797d57dfe4ed5a3bd0ebdda80eb83566bb9
Lina Adkins <Lina.Adkins@gmail.com>
2016-10-24 20:28:27 -0400

    Added Extensions folder and Transform Extensions cs file.

---
+++ Assets/Boiler-Tools/Extensions/TransformExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace BoilerTools.Extensions
+{
+    public static class TransformExtensions
+    {
+        /// <summary>
+        /// Destroy all children. Todo, time/alloc test foreach & clone destroy speeds. Testing in unity 5.4.2 on PC shows that the For loop is roughly twice as fast as the Foreach counterpart.
+        /// </summary>
+        public static void DestroyAllChildren(this Transform transform)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                GameObject.Destroy(transform.GetChild(i).gameObject);
+            }
+        }
+    }
+}
